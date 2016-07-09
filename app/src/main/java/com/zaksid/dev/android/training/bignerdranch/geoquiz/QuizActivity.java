c8c667b782d9bc444e590397a2061096e0f136e6
zaksid <thezaksid@gmail.com>
2016-07-10 00:45:18 +0300

    Chapter 1. Your First Android Application
    Add layout, buttons, toast notifications with mock.

--- app/src/main/java/com/zaksid/dev/android/training/bignerdranch/geoquiz/QuizActivity.java
+++ app/src/main/java/com/zaksid/dev/android/training/bignerdranch/geoquiz/QuizActivity.java
@@ -8,8 +8,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+    private Button trueButton;
+    private Button falseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,23 @@ public class QuizActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        trueButton = (Button) findViewById(R.id.true_button);
+        trueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+        falseButton = (Button) findViewById(R.id.false_button);
+        falseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT)
+                        .show();
             }
         });
     }
